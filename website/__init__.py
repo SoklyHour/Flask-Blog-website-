0311298915d6322d2865cb4b3c64c767b4eefbd6
Sokly Hour <hoursokly011@gmail.com>
2024-10-08 16:16:11 -0500

    add

--- website/__init__.py
+++ website/__init__.py
@@ -7,12 +7,7 @@ def create_app():
     app = Flask(__name__)
     app.config["SECRET_KEY"] = "helloworld"
 
-    @app.route("/")
-    def home():
-        return "<h1>Hello<h1>"
-    
-    @app.route("/profile")
-    def profile():
-        return "<h1>Profile<h1>"
+    from .views import views
+    app.register_blueprint(views, url_prefix="/")   
     
     return app
