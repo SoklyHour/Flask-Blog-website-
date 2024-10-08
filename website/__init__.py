719ceca99b1034304d59ef24365e31061d422696
Sokly Hour <hoursokly011@gmail.com>
2024-10-08 16:25:40 -0500

    add

--- website/__init__.py
+++ website/__init__.py
@@ -12,6 +12,6 @@ def create_app():
 
 
     app.register_blueprint(views, url_prefix="/")   
-    app.register_blueprint(views, url_prefix="/")  
+    app.register_blueprint(auth, url_prefix="/")  
     
     return app
