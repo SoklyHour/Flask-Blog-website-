088d28b87e482ddaeca72066fe962b90e5eba084
Sokly Hour <hoursokly011@gmail.com>
2024-10-08 19:42:50 -0500

    add

--- website/__init__.py
+++ website/__init__.py
@@ -8,26 +8,31 @@ DB_NAME = "database.db"
 
 def create_app():
     app = Flask(__name__)
-    app.config["SECRET_KEY"] = "helloworld"
-    app.config["SQL_ALCHEMY_DATABASE_URI"] = f'sqlite:///{DB_NAME}'
+    app.config['SECRET_KEY'] = "helloworld"
+    app.config['SQLALCHEMY_DATABASE_URI'] = f'sqlite:///{DB_NAME}'  # Fix the typo here
     db.init_app(app)
 
     from .views import views
     from .auth import auth
 
-    app.register_blueprint(views, url_prefix="/")   
-    app.register_blueprint(auth, url_prefix="/")  
+    app.register_blueprint(views, url_prefix="/")
+    app.register_blueprint(auth, url_prefix="/")
 
-    from .models import User    
+    from .models import User
 
     create_database(app)
 
     login_manager = LoginManager()
-    login_manager.login_view = "auth.view"
+    login_manager.login_view = "auth.login"
+    login_manager.init_app(app)
+
+    @login_manager.user_loader
+    def load_user(id):
+        return User.query.get(int(id))
 
     return app
 
 def create_database(app):
     if not path.exists("website/" + DB_NAME):
         db.create_all(app=app)
-        print("Create Database!")
+        print("Created database!")
